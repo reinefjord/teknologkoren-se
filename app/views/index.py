5a7019be7b9ca54f41c7806aa3fefc3713aacf64
Rupus Reinefjord <rupus@kltrst.se>
2016-06-21 14:23:42 +0200

    redirect to last page if page number too high
    this always annoys me if I for some reason inputs a too high page number
    on blogs and it gives me a 404 or empty page.

--- app/views/index.py
+++ app/views/index.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, url_for
 from app.models import Page, Post
+
 
 mod = Blueprint('index', __name__)
 
@@ -10,7 +11,13 @@ def index(page):
     index = Page.get(Page.name == 'index')
     posts = Post.select().where(
             Post.page == index).order_by(Post.timestamp.desc())
+
     pagination = posts.paginate(page, 5)
+
+    if not pagination:
+        last_page = (len(posts) // 5) + 1
+        return redirect(url_for('.index', page=last_page))
+
     has_next = True if posts.paginate(page+1, 5) else False
 
     return render_template('index.html',
