57ab55659f98fad15312ae58c38a22ad434feac6
Rupus Reinefjord <rupus@kltrst.se>
2016-11-02 15:23:13 +0100

    login required for all intranet views, skel views

--- app/views/intranet.py
+++ app/views/intranet.py
@@ -1,10 +1,25 @@
 from flask import Blueprint, render_template
-from flask_login import login_required
+from flask_login import current_user, login_required
 
-mod = Blueprint('intranet', __name__, url_prefix='/intranet/')
+mod = Blueprint('intranet', __name__, url_prefix='/intranet')
+
+
+@mod.before_request
+@login_required
+def before_request():
+    pass
 
 
 @mod.route('/')
-@login_required
 def index():
     return render_template('intranet.html')
+
+
+@mod.route('/profile/')
+def my_profile():
+    pass
+
+
+@mod.route('/profile/<int:id>')
+def profile(id):
+    pass
