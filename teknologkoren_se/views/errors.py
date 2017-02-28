cb819b8c798da3ed6a4b03bd733db3dd503b0c4d
Rupus Reinefjord <rupus@kltrst.se>
2017-02-28 20:35:26 +0100

    have to return the error code

--- teknologkoren_se/views/errors.py
+++ teknologkoren_se/views/errors.py
@@ -4,14 +4,14 @@ from teknologkoren_se import app
 
 @app.errorhandler(403)
 def forbidden(e):
-    return render_template('errors/403.html')
+    return render_template('errors/403.html'), 403
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('errors/404.html')
+    return render_template('errors/404.html'), 404
 
 
 @app.errorhandler(500)
 def server_error(e):
-    return render_template('errors/500.html')
+    return render_template('errors/500.html'), 500
